dafabfbd791d49f22e47978aafd2cff34d304b15
Dan <dangroza1988@gmail.com>
2022-03-05 10:00:11 +0200

    primele 2 ex

---
+++ 04_work/main.js
@@ -0,0 +1,15 @@
+// Create a function that takes two numbers as arguments and returns their sum.
+
+function sum(a, b){
+    return a + b;
+}
+console.log(sum(55, 80));
+
+// Write a function that takes an integer minutes and converts it to seconds.
+
+// a = variabila in minute
+
+function conv(a){
+    return a * 60;
+}
+console.log(conv(80));
