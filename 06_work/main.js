b8acd9f8b99b3ed71982df2feb781ffa1e938d45
dangroza88 <dangroza1988@gmail.com>
2022-03-17 23:27:43 +0200

    for fun

--- 06_work/main.js
+++ 06_work/main.js
@@ -1,12 +1,11 @@
 
-const lives = 3;
-const playGround = document.querySelector(".game-container");
+var lives = 3;
+var playGround = document.querySelector(".game-container");
 
-setInterval(() => {
-  
-}, 1000);
+
 
 let player = document.querySelector(".player")
+
 document.addEventListener("keydown", function (event) {
   // console.log("event= ", event)
   if (event.code === "ArrowDown") {
@@ -25,23 +24,75 @@ document.addEventListener("keydown", function (event) {
   }
 
   if (event.code === "ArrowRight") {
-    let oldLeft = parseInt(player.style.left) 
+    let oldLeft = parseInt(player.style.left)
     if (oldLeft < 370) {
-      player.style.left = `${oldLeft + 10}px` 
+      player.style.left = `${oldLeft + 10}px`
     }
   }
 
   if (event.code === "ArrowLeft") {
-    let oldLeft = parseInt(player.style.left) 
+    let oldLeft = parseInt(player.style.left)
     if (oldLeft > 0) {
-      player.style.left = `${oldLeft - 10}px` 
+      player.style.left = `${oldLeft - 10}px`
     }
   }
 
-
-  
   
 });
+
+
+var i = 0;
+var obsNum = setInterval(function () {
+  // console.log(event);
+  var obstaco = {
+    left: parseInt(Math.random() * 330),
+    // top: parseInt(Math.random() * 400)
+  }
+  var obsDiv = document.createElement("div");
+  playGround.appendChild(obsDiv);
+  obsDiv.classList.add("obs");
+  obsDiv.setAttribute("id", "obs" + i)
+  // obsDiv.setAttribute("style", "top: " + obstaco.top + "px; left: " + obstaco.left + "px;");
+  obsDiv.setAttribute("style", "top: 10px; left: " + obstaco.left + "px;");
+  console.log(obstaco);
+  i++;
+  if (i >= 5) {
+    clearInterval(obsNum);
+  }
+}, 1000);
+
+
+function overlap(){
+  let rect1 = player.getBoundingClientRect();
+
+  let obs0 = document.getElementById("obs0")
+  let rect2 = obs0.getBoundingClientRect();
+
+  let obs1 = document.getElementById("obs1")
+  let rect3 = obs1.getBoundingClientRect();
+
+  let obs2 = document.getElementById("obs2")
+  let rect4 = obs2.getBoundingClientRect();
+
+  let obs3 = document.getElementById("obs3")
+  let rect5 = obs3.getBoundingClientRect();
+
+  let obs4 = document.getElementById("obs4")
+  let rect6 = obs4.getBoundingClientRect();
+
+  let obsObject = {rect2, rect3, rect4, rect5, rect6};
+  var obsObjectKeys = Object.keys(obsObject);
+  console.log(obsObject);
+  console.log(obsObjectKeys);
+ 
+  debugger;
+  var overlap = !(rect1.right < rect2.left ||
+    rect1.left > rect2.right ||
+    rect1.bottom < rect2.top ||
+    rect1.top > rect2.bottom)
+    console.log(overlap);
+
+}
 
 // document.addEventListener("keydown", function (event){
 //   console.log(event);
@@ -58,19 +109,6 @@ document.addEventListener("keydown", function (event) {
 // });
 
 
-setInterval( function () {
-  // console.log(event);
-  var obstaco = {
-    left: parseInt(Math.random() * 370),
-    top: parseInt(Math.random() * 400)
-  }
-  var obsDiv = document.createElement("div");
-  playGround.appendChild(obsDiv);
-  obsDiv.classList.add("obs"); 
-  // obsDiv.setAttribute("style", "top: " + obstaco.top + "px; left: " + obstaco.left + "px;");
-  obsDiv.setAttribute("style", "top: 0px; left: " + obstaco.left + "px;");
-  console.log(obstaco);
-}, 1000);
 
 
 
@@ -85,5 +123,4 @@ function del() {
 
 
 
-// console.log(obsRandom())
 
