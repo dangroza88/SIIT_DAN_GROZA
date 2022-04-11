8c71461a5c50cd5645c7b8af33d72eef2e0fab70
dangroza88 <dangroza1988@gmail.com>
2022-04-11 11:53:32 +0300

    clean

--- 08_work/api.js
+++ 08_work/api.js
@@ -2,22 +2,22 @@ console.log("api up");
 const baseUrl = 'http://localhost:3000';
 
 const getTodos = async () => {
-    const response = await fetch(`${baseUrl}/todos`)
-    const todos = await response.json()
-    return todos
-  }
+  const response = await fetch(`${baseUrl}/todos`)
+  const todos = await response.json()
+  return todos
+}
 
 
-  const createToDoServer = async (newToDo) => {
-    const response = await fetch(`${baseUrl}/todos`, {
-      method: "POST",
-      headers: {
-        'Content-Type': 'application/json' 
-      },
-      body: JSON.stringify(newToDo)
-    })
-  
-    return response.json();
-  }
+const createToDoServer = async (newToDo) => {
+  const response = await fetch(`${baseUrl}/todos`, {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(newToDo)
+  })
+
+  return response.json();
+}
 
 
--- 08_work/mainRemove.js
+++ 08_work/mainRemove.js
@@ -1,14 +1,14 @@
 function getDeleteButton(todoId) {
-    const deleteButton = document.createElement("button")
-    deleteButton.innerHTML = "Delete"
-  
-    deleteButton.addEventListener("click", async function () {
-      await fetch(`${baseUrl}/todos/${todoId}`, {
-        method: "DELETE",
-      });
-      location.reload();
-  
-    })
-  
-    return deleteButton;
-  }
+  const deleteButton = document.createElement("button")
+  deleteButton.innerHTML = "Delete"
+
+  deleteButton.addEventListener("click", async function () {
+    await fetch(`${baseUrl}/todos/${todoId}`, {
+      method: "DELETE",
+    });
+    location.reload();
+
+  })
+
+  return deleteButton;
+}
