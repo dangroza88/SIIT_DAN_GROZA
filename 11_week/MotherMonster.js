53b4476173f58968b1e3c7bb862b92ae500a7e2d
dangroza88 <dangroza1988@gmail.com>
2022-05-02 13:35:34 +0300

    shooter done

--- 11_week/MotherMonster.js
+++ 11_week/MotherMonster.js
@@ -1,23 +1,24 @@
 
+import ElementMove from "./Movement.js";
 
 
-class Monster {
-    constructor(location) {
-    //   super();
+class Monster extends ElementMove {
+  constructor(location) {
+    super();
     // debugger
-      const monsterImg = document.createElement("img");
-      monsterImg.style.top = "0px";
-      monsterImg.style.left = "0px";
-      const randomNumber = Math.floor(Math.random() * 11) + 1;
-  
-      monsterImg.src = `./monster/monster${randomNumber}.svg`;
+    const monsterImg = document.createElement("img");
+    monsterImg.style.top = "0px";
+    monsterImg.style.left = "0px";
+    const randomNumber = Math.floor(Math.random() * 11) + 1;
+    monsterImg.src = `./monster/monster${randomNumber}.svg`
+    location.appendChild(monsterImg);
+    monsterImg.classList.add('monster')
+    this.element = monsterImg;
 
-    //   C:\Users\PC\Desktop\SIIT_dan.dev\SIIT_DAN_GROZA\monster\monster2.svg
-  
-      location.appendChild(monsterImg);
-      monsterImg.classList.add('monster')
-      this.element = monsterImg;
-    }
+
   }
+}
 
-  export default Monster;
+
+
+export default Monster;
--- 11_week/main.js
+++ 11_week/main.js
@@ -1,12 +1,57 @@
 console.log("1 2 3 ...go!");
+
 import Monster from './MotherMonster.js';
+import { gamecontainer } from './Movement.js';
 
-const gamecontainer = document.querySelector(".game-container");
-
+const scoreBox = document.querySelector(".score");
+let score = 0;
 
 // debugger;
+
 const monsterList = []
-for (let i = 0; i < 10; i++) {
-  const monster = new Monster(gamecontainer);
-  monsterList.push(monster);
+for (let i = 0; i < 30; i++) {
+    const monster = new Monster(gamecontainer);
+    monsterList.push(monster);
 }
+
+setInterval(function () {
+    for (let i = 0; i < monsterList.length; i++) {
+        const monster = monsterList[i];
+        moveMonster(monster);
+    }
+}, 500);
+
+gamecontainer.addEventListener("click", (e) => {
+    // bloodSpot.style.top = e.pageY + "px";
+    // bloodSpot.style.left = e.pageX + "px";
+    //   debugger
+    if (monsterList.some(monsterList => monsterList.element === e.target)) score++;
+    scoreBox.innerText = "SCORE: " + score;
+    console.log(monsterList);
+    console.log(e.target);
+    console.log("score" + score)
+});
+
+
+function moveMonster(myMonster) {
+    const movement = ['ArrowLeft', 'ArrowRight', 'ArrowUp', 'ArrowDown']
+    const randomIndex = Math.floor(Math.random() * movement.length);
+    const randomMove = movement[randomIndex];
+
+    if (randomMove === "ArrowDown") {
+        myMonster.moveDown()
+    }
+    if (randomMove === "ArrowLeft") {
+        myMonster.moveLeft()
+    }
+    if (randomMove === "ArrowRight") {
+        myMonster.moveRight()
+    }
+    if (randomMove === "ArrowUp") {
+        myMonster.moveUp()
+    }
+}
+
+
+
+
